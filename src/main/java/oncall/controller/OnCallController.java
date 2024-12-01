9de4df7fe908a6431273489a600dcc1f87525ba2
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-12-01 16:02:28 +0900

    feat: 잘못된 입력 시 에러 메세지 출력 후 재입력 받기

--- src/main/java/oncall/controller/OnCallController.java
+++ src/main/java/oncall/controller/OnCallController.java
@@ -1,6 +1,6 @@
 package oncall.controller;
 
-import oncall.domain.Worker;
+import java.util.function.Supplier;
 import oncall.domain.Workers;
 import oncall.domain.WorkingDay;
 import oncall.service.OnCallService;
@@ -13,27 +13,52 @@ public class OnCallController {
     private final OutputView outputView;
     private final OnCallService onCallservice;
 
-    public OnCallController(InputView inputView, OutputView outputView, OnCallService onCallservice) {
+    public OnCallController(InputView inputView, OutputView outputView,
+            OnCallService onCallservice) {
         this.inputView = inputView;
         this.outputView = outputView;
         this.onCallservice = onCallservice;
     }
 
     public void start() {
-        inputView.printMonthAndDayMessage();
-        String monthAndDay = inputView.monthAndDayInput();
-        WorkingDay workingDay = onCallservice.createWorkingDay(monthAndDay);
-
-        String weekWorkers = inputView.WeekendWorkWorkInput();
-        String holidayWorkers = inputView.holidayWorkInput();
-
-        Workers workers = onCallservice.createWorkers(weekWorkers);
-        for (Worker worker : workers.getWeekWorkers()) {
-            System.out.println(worker.getName());
-        }
-
-        onCallservice.addHolidayWorker(workers ,holidayWorkers);
+        handleMonthInput();
+        handleWorkers();
     }
 
+    private WorkingDay handleMonthInput() {
+        inputView.printMonthAndDayMessage();
+        return retryOnInvalidInput(() -> {
+            String monthAndDay = inputView.monthAndDayInput();
+            return onCallservice.createWorkingDay(monthAndDay);
+        });
+    }
+
+    /*
+    private WorkingDay handleCreateWorkingDay(String monthAndDay) {
+        WorkingDay workingDay = retryOnInvalidInput(
+                () -> onCallservice.createWorkingDay(monthAndDay));
+        return workingDay;
+    }
+
+     */
+    private Workers handleWorkers() {
+        return retryOnInvalidInput(() -> {
+            String weekWorkers = inputView.WeekendWorkWorkInput();
+            String holidayWorkers = inputView.holidayWorkInput();
+            Workers workers = onCallservice.createWorkers(weekWorkers);
+            onCallservice.addHolidayWorker(workers, holidayWorkers);
+            return workers;
+        });
+    }
+
+    private <T> T retryOnInvalidInput(Supplier<T> input) {
+        while (true) {
+            try {
+                return input.get();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
 
 }
